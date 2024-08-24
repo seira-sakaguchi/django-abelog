d4967b625e18e023456cbb37284c9d31a7d87594
seira-sakaguchi <sakaaaaaaaguchi@yahoo.co.jp>
2024-08-24 18:04:53 +0900

    画像アップロード済み(filezilla)

--- my_venv/myproject/myproject/settings.py
+++ my_venv/myproject/myproject/settings.py
@@ -3,7 +3,7 @@ from .settings_common import *
  
 DEBUG = True
  
-ALLOWED_HOSTS = ['34.235.34.95','sample.com']
+ALLOWED_HOSTS = ['abelog1900.com', 'www.abelog1900.com']
  
 STATIC_ROOT = '/usr/share/nginx/html/static'
-MEDIA_ROOT = '/usr/share/nginx/html/media'
+MEDIA_ROOT = '/usr/share/nginx/html/media' #filezillaでアップロードするディレクトリ
