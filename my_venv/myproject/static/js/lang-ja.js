a05ee2a2041afe9eec764d556f1158f6acacabba
seira-sakaguchi <sakaaaaaaaguchi@yahoo.co.jp>
2024-08-24 21:31:39 +0900

    Update .gitignore to exclude virtual environment directories

--- my_venv/myproject/static/js/lang-ja.js
+++
@@ -1,9 +0,0 @@
-jQuery.extend( jQuery.fn.pickadate.defaults, {
-    monthsFull: [ '1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月' ],
-    monthsShort: [ '1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月' ],
-    weekdaysFull: [ '日' , '月', '火', '水', '木', '金', '土'],
-    weekdaysShort: [ '日' ,  '月', '火', '水', '木', '金', '土'],
-    today: '本日',
-    clear: 'キャンセル',
-    format: 'yyyy年mm月dd日'
-});
