f8b0dcc9a690a7d92b746b47846433ce5cdcd188
seira-sakaguchi <sakaaaaaaaguchi@yahoo.co.jp>
2024-09-05 13:49:58 +0900

    メール設定

--- my_venv/myproject/myproject/settings.py
+++ my_venv/myproject/myproject/settings.py
@@ -11,8 +11,8 @@ STATIC_ROOT = '/usr/share/nginx/html/static'
 MEDIA_ROOT = '/usr/share/nginx/html/media' #filezillaでアップロードするディレクトリ
 
 #環境変数で書きたい
-EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
-EMAIL_HOST_USER = '${ses-smtp-user.20240830-195422}'
-EMAIL_HOST_PASSWORD = '${BAB7y1IYf9GF2rkd+L0db39OCRi++HoJNMlzYBli/Aux}'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+# EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
+# EMAIL_HOST_USER = '${ses-smtp-user.20240830-195422}'
+# EMAIL_HOST_PASSWORD = '${BAB7y1IYf9GF2rkd+L0db39OCRi++HoJNMlzYBli/Aux}'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
--- my_venv/myproject/myproject/settings_common.py
+++ my_venv/myproject/myproject/settings_common.py
@@ -157,7 +157,16 @@ ACCOUNT_LOGOUT_ON_GET = True
 ACCOUNT_EMAIL_SUBJECT_PREFIX = ''
 
 #デフォルトのメール送信元を設定
-DEFAULT_FROM_EMAIL = os.environ.get('FROM_EMAIL')
+# DEFAULT_FROM_EMAIL = os.environ.get('FROM_EMAIL')
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'abelog1900@gmail.com'
+EMAIL_HOST_PASSWORD = 'inoxjlnwtqdbbmvz'
+
 
 LOGGING = {
     'version': 1,  # 1固定
