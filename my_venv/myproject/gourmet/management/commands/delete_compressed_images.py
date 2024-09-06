d740ab6ef704c7dadfd39d959f693c58507c036e
seira-sakaguchi <sakaaaaaaaguchi@yahoo.co.jp>
2024-09-07 07:35:32 +0900

    debug

--- my_venv/myproject/gourmet/management/commands/delete_compressed_images.py
+++ my_venv/myproject/gourmet/management/commands/delete_compressed_images.py
@@ -1,17 +1,18 @@
-import os
+from django.core.management.base import BaseCommand
 from django.conf import settings
+import os
 
-def delete_compressed_images():
-    media_root = settings.MEDIA_ROOT
-    for root, dirs, files in os.walk(media_root):
-        for file in files:
-            if '_compressed' in file.lower():
-                file_path = os.path.join(root, file)
-                try:
-                    os.remove(file_path)
-                    print(f"Deleted {file_path}")
-                except Exception as e:
-                    print(f"Error deleting {file_path}: {str(e)}")
+class Command(BaseCommand):
+    help = 'Delete all compressed images from the media directory'
 
-if __name__ == "__main__":
-    delete_compressed_images()
+    def handle(self, *args, **kwargs):
+        media_root = settings.MEDIA_ROOT
+        for root, dirs, files in os.walk(media_root):
+            for file in files:
+                if '_compressed' in file.lower():
+                    file_path = os.path.join(root, file)
+                    try:
+                        os.remove(file_path)
+                        self.stdout.write(self.style.SUCCESS(f'Deleted {file_path}'))
+                    except Exception as e:
+                        self.stdout.write(self.style.ERROR(f'Error deleting {file_path}: {str(e)}'))
